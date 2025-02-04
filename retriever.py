86308839487f5859a290f17aecd04c5b2263b78f
Fabien <fabien.bernier@uni.lu>
2025-02-04 17:39:36 +0100

    [~] retriever has a main

--- retriever.py
+++ retriever.py
@@ -2,6 +2,7 @@ import arxiv
 import json
 from datetime import datetime
 
+
 # Define the categories
 categories = [
     "cs.AI",  # Computer Science - Artificial Intelligence
@@ -16,31 +17,33 @@ categories = [
     "nlin.AO"  # Nonlinear Sciences - Adaptation and Self-Organizing
 ]
 
-all_papers = []
-today = datetime.now().strftime("%Y%m%d%H%M")
 
-client = arxiv.Client()
+if __name__ == "__main__":
+    all_papers = []
+    today = datetime.now().strftime("%Y%m%d%H%M")
 
-for category in categories:
-    search = arxiv.Search(
-        query=f"cat:{category} AND submittedDate:[201801010600 TO {today}]",
-        max_results=200,
-        sort_by=arxiv.SortCriterion.Relevance
-    )
+    client = arxiv.Client()
 
-    print(f"Category {category}...")
-    i = 0
-    for i, result in enumerate(client.results(search)):
-        paper_data = {
-            "title": result.title,
-            "abstract": result.summary
-        }
-        all_papers.append(paper_data)
+    for category in categories:
+        search = arxiv.Search(
+            query=f"cat:{category} AND submittedDate:[201801010600 TO {today}]",
+            max_results=200,
+            sort_by=arxiv.SortCriterion.Relevance
+        )
 
-    print(f"Retrieved {i+1} papers")
+        print(f"Category {category}...")
+        i = 0
+        for i, result in enumerate(client.results(search)):
+            paper_data = {
+                "title": result.title,
+                "abstract": result.summary
+            }
+            all_papers.append(paper_data)
 
-# Write the papers data to a JSON file
-with open("arxiv_papers.json", "w", encoding='utf-8') as f:
-    json.dump(all_papers, f, ensure_ascii=False)
+        print(f"Retrieved {i+1} papers")
 
-print(f"Successfully saved {len(all_papers)} papers' data to arxiv_papers.json")
+    # Write the papers data to a JSON file
+    with open("arxiv_papers.json", "w", encoding='utf-8') as f:
+        json.dump(all_papers, f, ensure_ascii=False)
+
+    print(f"Successfully saved {len(all_papers)} papers' data to arxiv_papers.json")
